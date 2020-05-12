e33afb379cb20ee96d9b0c4fa571bed8a680d14d
Gabriel Zsigmond <gaazsig@gmail.com>
2020-05-11 23:37:50 -0300

    getting number of contributions

---
+++ api/repo-contribution.js
@@ -0,0 +1,16 @@
+const { getContributions } = require("../githubAPI/calls");
+
+module.exports = async (req, res) => {
+  const start = new Date();
+  let numberOfContributions = 0;
+  const { repo, user } = req.query;
+  const contributions = await getContributions(repo, user);
+  for (let i = 0; i < contributions.data.length; i++) {
+    if (contributions.data[i].login === user) {
+      numberOfContributions = contributions.data[i].contributions;
+    }
+  }
+  const end = new Date();
+  console.log(`seconds elapsed = ${Math.floor((end - start) / 1000)}`);
+  res.status(200).json({ allContributions: numberOfContributions });
+};
